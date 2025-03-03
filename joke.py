adbd2497d07fa63d0e5f56860169a58041d55f8f
Himanshu26112002 <131147306+Himanshu26112002@users.noreply.github.com>
2025-03-03 14:34:04 +0530

    Delete joke.py

--- joke.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-joke = requests.get('https://official-joke-api.appspot.com/random_joke').json()
-setup = joke['setup']
-punchline = joke['punchline']
-print(setup)
-print(punchline)
